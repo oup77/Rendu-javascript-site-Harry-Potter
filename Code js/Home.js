7f2c439775ef929e8ac38f76bf7a442510f50480
oup77 <vaclelenmenttin77@gmail.com>
2024-02-11 12:17:13 +0100

    Tabs + dark mode

--- Code js/Home.js
+++ Code js/Home.js
@@ -28,6 +28,9 @@ darkMode.addEventListener("click", function () {
   let cardlign = document.querySelector("#section2");
   let footer = document.querySelector("footer");
   let navBoutons = document.querySelectorAll(".navlink");
+  let tabsBack = document.querySelector(".tabs");
+  let listTabBack = document.querySelector(".list-tab");
+  let contentsColor = document.querySelectorAll(".content");
 
   if (darkMode.classList.contains("dark")) {
     navBarre.classList.add("dark");
@@ -39,6 +42,11 @@ darkMode.addEventListener("click", function () {
     navBoutons.forEach(function (navBouton) {
       navBouton.classList.add("dark");
     });
+    tabsBack.classList.add("dark");
+    listTabBack.classList.add("dark");
+    contentsColor.forEach(function (contentcolor) {
+      contentcolor.classList.add("dark");
+    });
   } else {
     navBarre.classList.remove("dark");
     cardChoices.forEach(function (cardChoice) {
@@ -49,5 +57,46 @@ darkMode.addEventListener("click", function () {
     navBoutons.forEach(function (navBouton) {
       navBouton.classList.remove("dark");
     });
+    tabsBack.classList.remove("dark");
+    listTabBack.classList.remove("dark");
+    contentsColor.forEach(function (contentcolor) {
+      contentcolor.classList.remove("dark");
+    });
   }
+});
+
+let tabs = document.querySelectorAll(".tab");
+
+tabs.forEach(function (tab) {
+  tab.addEventListener("click", function () {
+    tabs.forEach(function (tab) {
+      tab.classList.remove("tab-active");
+    });
+    let contents = document.querySelectorAll(".content");
+    contents.forEach(function (content) {
+      content.classList.remove("active");
+    });
+    this.classList.add("tab-active");
+
+    if (this.classList.contains("tab-objectif")) {
+      let objectif = document.querySelector(".objectif");
+      objectif.classList.add("active");
+    }
+    if (this.classList.contains("tab-pourquoi")) {
+      let pourquoi = document.querySelector(".pourquoi");
+      pourquoi.classList.add("active");
+    }
+    if (this.classList.contains("tab-soutenir")) {
+      let soutenir = document.querySelector(".soutenir");
+      soutenir.classList.add("active");
+    }
+    if (this.classList.contains("tab-sinon")) {
+      let sinon = document.querySelector(".sinon");
+      sinon.classList.add("active");
+    }
+    if (this.classList.contains("tab-ane")) {
+      let ane = document.querySelector(".ane");
+      ane.classList.add("active");
+    }
+  });
 });
